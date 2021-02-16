0189fcfa20b8aa78deaddf25cb359511896a0f87
flaggs_hwang <hwang.mansuk@flaggs.co.jp>
2021-02-16 09:35:38 +0900

    最初のアップロード、unityプロジェクトファイル

---
+++ EarthWalker/Assets/Joystick Pack/Examples/JoystickPlayerExample.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoystickPlayerExample : MonoBehaviour
+{
+    public float speed;
+    public VariableJoystick variableJoystick;
+    public Rigidbody rb;
+
+    [SerializeField]
+    private ForceMode _fMode = ForceMode.VelocityChange;
+
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * variableJoystick.Vertical + Vector3.right * variableJoystick.Horizontal;
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, _fMode);
+        //rb.AddForce(direction * speed * Time.fixedDeltaTime, _fMode);
+    }
+}
